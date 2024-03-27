cec50593f9f80dea58bf903f9f500b1392d36b8c
Lexis404 <121729689+Webster-Botz@users.noreply.github.com>
2024-03-27 18:51:49 +0100

    Add files via upload

--- src/Handlers/Message.js
+++ src/Handlers/Message.js
@@ -1,129 +1,184 @@
-const { serialize } = require('../lib/WAclient')
-const { getStats } = require('../lib/stats')
-const chalk = require('chalk')
-const emojiStrip = require('emoji-strip')
-const axios = require('axios')
+const { HelperPrompt, transcribe, wikipedia, google, countryTime, weather, toSpeech } = require('../lib/Helper')
+const { Keys, complement } = require('../lib/Messages')
+const { serialize, decodeJid } = require('../lib/WAclient')
+const { GoogleGenerativeAI } = require('@google/generative-ai')
+const { audioToSlice, audioMerge } = require('audio-slicer')
+const emojis = require('emoji-strip')
+let helper = ''
 
-module.exports = MessageHandler = async (messages, client) => {
-    try {
-        if (messages.type !== 'notify') return
-        let M = serialize(JSON.parse(JSON.stringify(messages.messages[0])), client)
-        if (!M.message) return
-        if (M.key && M.key.remoteJid === 'status@broadcast') return
-        if (M.type === 'protocolMessage' || M.type === 'senderKeyDistributionMessage' || !M.type || M.type === '')
-            return
+module.exports = async ({ messages }, client) => {
+    const M = serialize(messages[0], client)
+    if (!M.message || ['protocolMessage', 'senderKeyDistributionMessage'].includes(M.type) || !M.type) return null
+    let { isGroup, sender, from, body } = M
+    const result = isGroup ? await client.groupMetadata(from) : {}
+    const admins = isGroup ? result.participants.filter(({ admin }) => admin).map(({ id }) => id) : []
+    client.isAdmin = isGroup && admins?.includes(decodeJid(client.user?.id))
+    const args = body.trim().split(' ')
+    const isCmd = args[0].startsWith(client.prefix)
+    let info = await client.daily.get(M.sender)
 
-        const { isGroup, sender, from, body } = M
-        const gcMeta = isGroup ? await client.groupMetadata(from) : ''
-        const gcName = isGroup ? gcMeta.subject : ''
-        const args = body.trim().split(/ +/).slice(1)
-        const isCmd = body.startsWith(client.prefix)
-        const cmdName = body.slice(client.prefix.length).trim().split(/ +/).shift().toLowerCase()
-        const arg = body.replace(cmdName, '').slice(1).trim()
-        const flag = args.filter((arg) => arg.startsWith('--'))
-        const groupMembers = gcMeta?.participants || []
-        const groupAdmins = groupMembers.filter((v) => v.admin).map((v) => v.id)
-        const ActivateMod = (await client.DB.get('mod')) || []
-        const ActivateChatBot = (await client.DB.get('chatbot')) || []
-        const banned = (await client.DB.get('banned')) || []
-
-        //Antilink
-        await antilink(client, M, groupAdmins, ActivateMod, isGroup, sender, body, from)
-
-        //Banned system
-        if (banned.includes(sender)) return M.reply('You are banned from using the bot')
-
-        //Ai chat
-        await ai_chat(client, M, isGroup, isCmd, ActivateChatBot, body, from)
-
-        // Logging Message
-        client.log(
-            `${chalk[isCmd ? 'red' : 'green'](`${isCmd ? '~EXEC' : '~RECV'}`)} ${
-                isCmd ? `${client.prefix}${cmdName}` : 'Message'
-            } ${chalk.white('from')} ${M.pushName} ${chalk.white('in')} ${isGroup ? gcName : 'DM'} ${chalk.white(
-                `args: [${chalk.blue(args.length)}]`
-            )}`,
-            'yellow'
-        )
-
-        if (!isCmd) return
-        const command =
-            client.cmd.get(cmdName) || client.cmd.find((cmd) => cmd.aliases && cmd.aliases.includes(cmdName))
-
-        if (!command) return M.reply('No such command found! BAKA')
-        if (!groupAdmins.includes(sender) && command.category == 'moderation')
-            return M.reply('This command can only be used by group or community admins')
-        if (!groupAdmins.includes(client.user.id.split(':')[0] + '@s.whatsapp.net') && command.category == 'moderation')
-            return M.reply('This command can only be used when bot is admin')
-        if (!isGroup && command.category == 'moderation') return M.reply('This command is ment to use in groups')
-        if (!client.mods.includes(sender.split('@')[0]) && command.category == 'dev')
-            return M.reply('This command only can be accessed by the mods')
-        command.execute(client, flag, arg, M)
-
-        //Experiance
-        await experience(client, sender, M, from, command)
-    } catch (err) {
-        client.log(err, 'red')
-    }
-}
-
-const antilink = async (client, M, groupAdmins, ActivateMod, isGroup, sender, body, from) => {
-    // Antilink system
-    if (
-        isGroup &&
-        ActivateMod.includes(from) &&
-        groupAdmins.includes(client.user.id.split(':')[0] + '@s.whatsapp.net') &&
-        body
-    ) {
-        const groupCodeRegex = body.match(/(https?:\/\/)?chat\.whatsapp\.com\/(?:invite\/)?([\w\d]*)/)
-        if (groupCodeRegex && groupCodeRegex.length === 2 && !groupAdmins.includes(sender)) {
-            const groupCode = groupCodeRegex[1]
-            const groupNow = await client.groupInviteCode(from)
-
-            if (groupCode !== groupNow) {
-                await client.sendMessage(from, { delete: M.key })
-                //return await client.groupParticipantsUpdate(from, [sender], 'remove')
-                M.reply('Successfully deleted link!!!!')
+    const public = [isCmd, M.key.fromMe, !isGroup]
+    if (!public.some(Boolean)) {
+        const botJid = decodeJid(client.user.id)
+        if (M.quoted || M.mentions.length) {
+            const mentioned = M.quoted ? [M.quoted.participant] : M.mentions
+            if (mentioned.includes(botJid)) {
+                if (Keys.includes(M.type)) return void M.reply(complement(M.type))
+                if (!body) return void M.reply('Go dm')
+                const text = body.replace(/@\d+/g, '')
+                let result = await getHelper(client.apiKey, text)
+                if (!/^{\s*".*"\s*}$/.test(result)) result = '{ "normal": null }'
+                const type = JSON.parse(result)
+                return void (await getGemini(M, client, text, info?.voice))
             }
         }
     }
+
+    const conditions = [isCmd, isGroup, M.key.fromMe]
+    if (!conditions.some(Boolean)) {
+        if (Keys.includes(M.type)) return void M.reply(complement(M.type))
+        if (M.type === 'imageMessage' && !M.message.imageMessage.caption) {
+            helper = await client.utils.ocrImage(await M.download())
+            const details = `Provide a concise and interesting response based on the text in the image. Try to use emojis in your response. Avoid using the phrase 'Image Interpretation' and provide the description directly without any additional introductory statements. If the text on the image is a question or math problem, solve it. ${M.message?.imageMessage?.caption}`
+            return void (await getGemini(M, client, details))
+        }
+
+        if (M.type === 'audioMessage') {
+            const voice = M.message?.audioMessage?.ptt
+            await M.reply(voice ? '👩🏻👂🎧' : '👩🏻🎧✍️')
+            if (!voice) {
+                let text = 'Write a Quick and Short Summary of text below:\n\n'
+                const duration = M.message?.audioMessage?.seconds
+                if (duration > 600) return void M.reply('You are only allowed to use audio less than 10 minutes')
+                if (duration > 75) {
+                    const audios = await audioToSlice(await M.download(), 75)
+                    if (!audios || !audios.length) return void M.reply('An error occurred')
+                    if (audios.length) {
+                        const total = audios.length
+                        for (let i = 0; i < total; i++) {
+                            const result = await transcribe(audios[i], client)
+                            text += result + '\n'
+                            await M.reply(`🎙️ *${1 + i}/${total}* ▶️ _"${result}"_`)
+                        }
+                    }
+                    return void (await getGemini(M, client, text))
+                }
+                const response = await transcribe(await M.download(), client)
+                await M.reply(`🎙️ *1/1* ▶️ _"${response}"_`)
+                text += response
+                return void (await getGemini(M, client, text))
+            }
+            body = await transcribe(await M.download(), client)
+            await M.reply(`I heard you saying 👩🏻👂🎧\n\n _"${body}_"`)
+        }
+
+
+        if (!body) return void null
+        let result = await getHelper(client.apiKey, body)
+        if (!/^{(\s*".*"\s*:\s*".*"\s*)}$/.test(result)) result = '{ "normal": null }'
+        const type = JSON.parse(result)
+        if (type.google) {
+            helper = await google(type.google)
+        } else if (type.time) {
+            helper = await countryTime(type.time)
+        } else if (type.weather) {
+            helper = await weather(type.weather)
+        } else if (type.voice) {
+            info.voice = type.voice
+            helper = type.voice ? '🟩 Enable' : '🟥 Disable'
+        }
+        return void (await getGemini(M, client, body, info?.voice))
+    }
+
+    if (!args[0] || !args[0].startsWith(client.prefix))
+        return void client.log(
+            `${chalk.cyanBright('Message')} from ${chalk.yellowBright(M.pushName)} in ${chalk.blueBright(
+                result.subject || 'DM'
+            )}`
+        )
 }
 
-const ai_chat = async (client, M, isGroup, isCmd, ActivateChatBot, body, from) => {
-    // AI chatting using
-    if (M.quoted?.participant) M.mentions.push(M.quoted.participant)
-    if (
-        M.mentions.includes(client.user.id.split(':')[0] + '@s.whatsapp.net') &&
-        !isCmd &&
-        isGroup &&
-        ActivateChatBot.includes(from)
-    ) {
-        const text = await axios.get(`https://api.simsimi.net/v2/?text=${emojiStrip(body)}&lc=en&cf=true`)
-        M.reply(body == 'hi' ? `Hey ${M.pushName} whats up?` : text.data.messages[0].text)
+const createSpeech = async (client, text) => {
+    const audios = await toSpeech(text)
+    if (!audios.length) return 'Unable to make long text as audio'
+    const audio = await audioMerge(audios)
+    const buffer = await client.utils.mp3ToOpus(audio)
+    return buffer
+}
+
+const getHelper = async (apiKey, content) => {
+    if (!apiKey) return null
+    const model = new GoogleGenerativeAI(apiKey).getGenerativeModel({ model: 'gemini-pro' })
+    try {
+        const messages = [
+            { role: 'user', parts: [{ text: `System Prompt: ${HelperPrompt}` }] },
+            { role: 'model', parts: [{ text: 'Understood.' }] }
+        ]
+        const chat = model.startChat({
+            history: messages,
+            generationConfig: {
+                maxOutputTokens: 4096
+            }
+        })
+        const { response } = await chat.sendMessage(content)
+        return response.text().replace(/\```/g, '')
+    } catch (err) {
+        console.log(err.message)
+        return '{ "normal": null }'
     }
 }
 
-const experience = async (client, sender, M, from, command) => {
-    //Will add exp according to the commands
-    await client.exp.add(sender, command.exp)
-
-    //Level up
-    const level = (await client.DB.get(`${sender}_LEVEL`)) || 0
-    const experience = await client.exp.get(sender)
-    const { requiredXpToLevelUp } = getStats(level)
-    if (requiredXpToLevelUp > experience) return null
-    await client.DB.add(`${sender}_LEVEL`, 1)
-    client.sendMessage(
-        from,
-        {
-            video: {
-                url: 'https://media.tenor.com/msfmevhmlDAAAAPo/anime-chibi.mp4'
-            },
-            caption: `Congratulations you leveled up from *${level} ---> ${level + 1}* 🎊`,
-            gifPlayback: true
-        },
-        {
-            quoted: M
+const getGemini = async (M, client, context, voice = false) => {
+    if (!client.apiKey) return null
+    const model = new GoogleGenerativeAI(client.apiKey).getGenerativeModel({ model: 'gemini-pro' })
+    if (helper) helper = `\n\nHelper: ${helper}`
+    try {
+        const messages = (await client.messagesMap.get(M.from)) || []
+        const content = `UTC: ${new Date().toUTCString()}\nName: ${
+            M.username
+        }\nMessage: ${context.trim()}\n\n ${helper}`
+        const Systemprompt = `System Prompt: You are Ai, a friendly WhatsApp Text and Voice based AI Bot developed for the purpose of helping or solving any waec questions. Always give a very short & precise response not more than 240 words. You are updated up-to ${new Date().getFullYear()}, Always use emojis in your response.
+                    To get any information, Waec Answers, exchanges rate, livescore, get media link url or upload medias, football realtime update, latest news, internet search, today's information, weather, time, google search, send images, stickers etc, You must always use the context provided by the Helper below the user's message to provide a more updated and correct response (Any information provided by the helper is correct and upto date, so never dispute it and never mention 'Helper' word in your response).`
+        if (!messages.length) {
+            messages.push({ role: 'user', parts: [{ text: Systemprompt }] })
+            messages.push({ role: 'model', parts: [{ text: 'Understood.' }] })
         }
-    )
+        if (messages[0].parts[0].text !== Systemprompt) messages[0].parts[0].text = Systemprompt
+        const chat = model.startChat({
+            history: messages,
+            generationConfig: {
+                maxOutputTokens: 4096
+            }
+        })
+        const tokens = await displayChatTokens(model, chat, content)
+        const { response } = await chat.sendMessage(content)
+        const historyMessages = await chat.getHistory()
+        console.log(tokens, historyMessages.length)
+        if (tokens > 3800 && historyMessages.length > 6) historyMessages.splice(2, 6)
+        await client.messagesMap.set(M.from, historyMessages)
+        helper = ''
+        const text = response.text().replace(new RegExp(`^${client.name}: `), '')
+        if (voice === 'true') {
+            const audio = await createSpeech(client, emojis(text))
+            if (Buffer.isBuffer(audio)) {
+                await M.status('recording')
+                return void (await client.sendMessage(M.from, { audio }, { quoted: M }))
+            }
+        }
+        await M.status('composing')
+        return void (await M.reply(text || 'Well...'))
+    } catch (error) {
+        console.log(error.message)
+        return void (await M.reply(
+            error?.response?.data?.error?.message ?? 'An error occurred while processing the request.'
+        ))
+    }
+}
+
+const displayChatTokens = async (model, chat, text) => {
+    const history = await chat.getHistory()
+    const message = { role: 'user', parts: [{ text }] }
+    const contents = [...history, message]
+    const { totalTokens } = await model.countTokens({ contents })
+    return totalTokens
 }
